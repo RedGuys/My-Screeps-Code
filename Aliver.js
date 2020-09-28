3313a913f20afcaa84480c62269445bcf834264e
Ilya <admin@redguy.ru>
2020-09-28 16:40:44 +0300

    Update live bots

--- Aliver.js
+++ Aliver.js
@@ -7,21 +7,27 @@ var Repairer = [WORK, WORK, CARRY, CARRY, MOVE, MOVE];
 
 module.exports.check = function() {
 	var creeps = [{"name":"Harvester1","type":Standart},
-			{"name":"Harvester2","type":Harvester},
-			{"name":"Harvester3","type":Standart},
-			{"name":"ControlerKeeper1","type":[WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE]}, //550
-			{"name":"ControlerKeeper2","type":[WORK, CARRY, CARRY, MOVE, MOVE]},
-			{"name":"Builder1","type":Builder},
-			{"name":"Builder2","type":Builder},
-			{"name":"Defender1","type":Defender},
-			{"name":"Healer1","type":Healer},
-			{"name":"Fixick1","type":Repairer},
-			{"name":"Fixick2","type":Repairer}];
+        {"name":"Harvester2","type":Harvester},
+        {"name":"Harvester3","type":Standart},
+        {"name":"ControlerKeeper1","type":[WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE]}, //550
+        {"name":"ControlerKeeper2","type":[WORK, CARRY, CARRY, MOVE, MOVE]},
+        {"name":"ControlerKeeper3","type":[WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE]}, //800
+        {"name":"Builder1","type":Builder},
+        {"name":"Builder2","type":Standart},
+        {"name":"Builder3","type":Builder},
+        //{"name":"Defender1","type":Defender},
+        //{"name":"Healer1","type":Healer},
+        {"name":"Fixick1","type":Repairer},
+        {"name":"Fixick2","type":Repairer},
+        {"name":"Cleaner1","type":Standart}];
 	for(var i = 0;i < creeps.length; i++) {
 		if(!Game.creeps[creeps[i].name]) {
-			if(Game.spawns["Home"].createCreep(creeps[i].type,creeps[i].name) == 0) {
+		    let res = Game.spawns["Home"].createCreep(creeps[i].type,creeps[i].name);
+			if(res === 0) {
 				Game.creeps[creeps[i].name].move(TOP);
-			}
+			} else {
+			    console.log(res);
+            }
 		}
 	}
 }

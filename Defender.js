9b12a1824dd1f677b85263bf5c4d573ba5bb3bf4
Ilya <admin@redguy.ru>
2020-09-28 16:42:11 +0300

    Update Defender

--- Defender.js
+++ Defender.js
@@ -1,12 +1,17 @@
 module.exports.work = function(creep) {
-	try {
-		const target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-		if(target) {
-			if(creep.attack(target) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(target);
-			}
-		}
-	} catch(e) {
-		console.log("Defender not found");
-	}
+    if(creep === undefined) {
+        if(require("./config").foundlog) {
+            console.log("Defender not found");
+        }
+    } else {
+        try {
+            const target = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            if (target) {
+                if (creep.attack(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+        } catch (e) {
+        }
+    }
 }

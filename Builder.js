524a411a8d8b4dc93ad8e2a446ada50227a1ec7e
Ilya <admin@redguy.ru>
2020-09-28 16:41:26 +0300

    Update builder

--- Builder.js
+++ Builder.js
@@ -1,31 +1,35 @@
 var positions = require("./positions");
 
 module.exports.work = function(creep) {
-	try {
-		if(creep.memory.fulled) {
-			if(creep.carry.energy == 0) {
-				creep.memory.fulled = false;
-			} else {
-				const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-				if(target) {
-					if(creep.build(target) == ERR_NOT_IN_RANGE) {
-						creep.moveTo(target);
-					}
-				}
-			}
-		} else {
-			if(creep.carry.energy < creep.carryCapacity) {
-				var sources = creep.room.find(FIND_SOURCES_ACTIVE);
-				var source = positions.getNearest(creep.pos,sources);
-				if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(source);
-				}
-			}
-			else {
-				creep.memory.fulled = true;
-			}
-		}
-	} catch(e) {
-		console.log("Builder not found");
-	}
+    if(creep === undefined) {
+        if(require("./config").foundlog) {
+            console.log("Builder not found");
+        }
+    } else {
+        try {
+            if (creep.memory.fulled) {
+                if (creep.carry.energy === 0) {
+                    creep.memory.fulled = false;
+                } else {
+                    const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+                    if (target) {
+                        if (creep.build(target) === ERR_NOT_IN_RANGE) {
+                            creep.moveTo(target);
+                        }
+                    }
+                }
+            } else {
+                if (creep.carry.energy < creep.carryCapacity) {
+                    var sources = creep.room.find(FIND_SOURCES_ACTIVE);
+                    var source = positions.getNearest(creep.pos, sources);
+                    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(source);
+                    }
+                } else {
+                    creep.memory.fulled = true;
+                }
+            }
+        } catch (e) {
+        }
+    }
 }

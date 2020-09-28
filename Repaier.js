e22a90b22c654809b3c1828332030f313551c040
Ilya <admin@redguy.ru>
2020-09-28 16:43:12 +0300

    Update Repaier

--- Repaier.js
+++ Repaier.js
@@ -1,36 +1,40 @@
 var positions = require("./positions");
 
-module.exports.work = function(creep) {
-	try {
-		if(creep.memory.fulled) {
-			if(creep.carry.energy == 0) {
-				creep.memory.fulled = false;
-			} else {
-				const targets = creep.room.find(FIND_STRUCTURES, {
-					filter: object => object.hits < object.hitsMax
-				});
+module.exports.work = function(creep,i) {
+    if(creep === undefined) {
+        if(require("./config").foundlog) {
+            console.log("Defender not found");
+        }
+    } else {
+        try {
+            if (creep.memory.fulled) {
+                if (creep.carry.energy == 0) {
+                    creep.memory.fulled = false;
+                } else {
+                    const targets = creep.room.find(FIND_STRUCTURES, {
+                        filter: object => object.hits < object.hitsMax
+                    });
 
-				targets.sort((a,b) => (b.hitsMax / b.hits) - (a.hitsMax / a.hits));
+                    targets.sort((a, b) => (b.hitsMax / b.hits) - (a.hitsMax / a.hits));
 
-				if(targets.length > 0) {
-					if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
-						creep.moveTo(targets[0]);
-					}
-				}
-			}
-		} else {
-			if(creep.carry.energy < creep.carryCapacity) {
-				var sources = creep.room.find(FIND_SOURCES_ACTIVE);
-				var source = positions.getNearest(creep.pos,sources);
-				if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(source);
-				}
-			}
-			else {
-				creep.memory.fulled = true;
-			}
-		}
-	} catch(e) {
-		console.log("Fixick not found");
-	}
+                    if (targets.length > 0) {
+                        if (creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(targets[0]);
+                        }
+                    }
+                }
+            } else {
+                if (creep.carry.energy < creep.carryCapacity) {
+                    var sources = creep.room.find(FIND_SOURCES_ACTIVE);
+                    var source = positions.getNearest(creep.pos, sources);
+                    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(source);
+                    }
+                } else {
+                    creep.memory.fulled = true;
+                }
+            }
+        } catch (e) {
+        }
+    }
 }

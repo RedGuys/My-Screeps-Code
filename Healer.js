a16c42e8fb442298a8e6424b879f198894eedc24
Ilya <admin@redguy.ru>
2020-09-28 16:42:48 +0300

    Update Healer

--- Healer.js
+++ Healer.js
@@ -1,16 +1,21 @@
 module.exports.work = function(creep) {
-	try {
-		const target = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
-			filter: function(object) {
-				return object.hits < object.hitsMax;
-			}
-		});
-		if(target) {
-			if(creep.heal(target) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(target);
-			}
-		}
-	} catch(e) {
-		console.log("Healer not found");
-	}
+    if(creep === undefined) {
+        if(require("./config").foundlog) {
+            console.log("Defender not found");
+        }
+    } else {
+        try {
+            const target = creep.pos.findClosestByRange(FIND_MY_CREEPS, {
+                filter: function (object) {
+                    return object.hits < object.hitsMax;
+                }
+            });
+            if (target) {
+                if (creep.heal(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+        } catch (e) {
+        }
+    }
 }
